ee8139391184f38ad9a7f24e3704b4066b31b025
techakki <akshaykmr869@gmail.com>
2018-12-28 16:14:08 +0530

    setup reducer as per actions

---
+++ src/state_management/reducers/index.js
@@ -0,0 +1,32 @@
+import {
+    ADD_TASK,
+    REMOVE_TASK,
+    REMOVE_ALL
+} from "../constants";
+
+function addTask(task) {
+    return {
+        id: Math.random(),
+        name: task
+    };
+}
+
+function removeTaskById(state, id) {
+    return state.filter(task => task.id !== id);
+}
+
+export const reducer = (state = [], action) => {
+    switch(action.type) {
+        case ADD_TASK:
+            return [
+                ...state,
+                addTask(action.payload)
+            ];
+        case REMOVE_TASK:
+            return removeTaskById(state, action.payload);
+        case REMOVE_ALL:
+            return [];
+        default:
+            return state;
+    };
+};
