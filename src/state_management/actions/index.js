3161f2ef20e42df51407e32195fbfd0d2266f6ce
techakki <akshaykmr869@gmail.com>
2018-12-28 16:02:58 +0530

    setup minimal actions

---
+++ src/state_management/actions/index.js
@@ -0,0 +1,25 @@
+import {
+    ADD_TASK,
+    REMOVE_TASK,
+    REMOVE_ALL
+} from "../constants";
+
+export const addTask = task => {
+    return {
+        type: ADD_TASK
+        payload: task
+    };
+};
+
+export const removeTask = id => {
+    return {
+        type: REMOVE_TASK,
+        payload: id
+    };
+};
+
+export const removeAllTasks = () => {
+    return {
+        type: REMOVE_ALL
+    }
+};
